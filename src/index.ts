05827de22dc7b75ab05767e9d0070819c37e252f
ProKoKos <prokos@list.ru>
2025-12-24 20:53:57 +0300

    Поправка зависимостей Докер

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
+import path from 'path';
 import { createServer } from 'http';
 import { WebSocketServer } from 'ws';
 
